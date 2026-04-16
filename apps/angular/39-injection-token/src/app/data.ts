6403516b90c6f643fd0f422e74eecc4b7222d3e5
slimabdi <36376210+slimabdi@users.noreply.github.com>
2026-04-16 12:36:17 +0200

    feat: 39-injection-token

--- apps/angular/39-injection-token/src/app/data.ts
+++ apps/angular/39-injection-token/src/app/data.ts
@@ -1,1 +1,8 @@
-export const DEFAULT_TIMER = 1000;
+import { InjectionToken } from '@angular/core';
+
+const DEFAULT_TIMER = 1000;
+
+export const TIMER_TOKEN = new InjectionToken<number>('TIMER_TOKEN', {
+  providedIn: 'root',
+  factory: () => DEFAULT_TIMER,
+});
